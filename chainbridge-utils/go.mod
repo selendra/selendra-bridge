17bd4d97cc00d7b51497019d9ce16dac33fb261c
laynath242@gmail.com <laynath242@gmail.com>
2021-09-15 12:58:23 +0700

    add chainbridge-utils

---
+++ chainbridge-utils/go.mod
@@ -0,0 +1,13 @@
+module github.com/selendra/selendra-bridge/chainbridge-utils
+
+go 1.13
+
+require (
+	github.com/ChainSafe/log15 v1.0.0
+	github.com/ethereum/go-ethereum v1.10.6
+	github.com/prometheus/client_golang v1.4.1
+	github.com/prometheus/procfs v0.0.10 // indirect
+	github.com/selendra/selendra-bridge/go-substrate-rpc-client/v3 v3.0.0-20210915054013-020acc2ab7e5
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
+	golang.org/x/sys v0.0.0-20210521203332-0cec03c779c1 // indirect
+)
---
+++ chainbridge-utils/metrics/health/health.go
@@ -0,0 +1,89 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package health
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"net/http"
+	"path"
+	"time"
+
+	"github.com/selendra/selendra-bridge/chainbridge-utils/core"
+	"github.com/selendra/selendra-bridge/chainbridge-utils/msg"
+	log "github.com/ChainSafe/log15"
+)
+
+type httpMetricServer struct {
+	port         int
+	blockTimeout int // After this duration (seconds) with no change in block height a chain will be considered unhealthy
+	chains       map[string]core.Chain
+	stats        map[string]*ChainStats
+}
+
+type ChainStats struct {
+	ChainId     msg.ChainId `json:"chainId"`
+	Height      *big.Int    `json:"height"`
+	LastUpdated time.Time   `json:"lastUpdated"`
+}
+
+// NewHealthServer creates a new server with unique endpoints for each chain based on chain name.
+func NewHealthServer(port int, chains []core.Chain, blockTimeout int) *httpMetricServer {
+	chainMap := make(map[string]core.Chain)
+	for _, c := range chains {
+		chainMap[c.Name()] = c
+	}
+
+	return &httpMetricServer{
+		port:         port,
+		chains:       chainMap,
+		blockTimeout: blockTimeout,
+		stats:        make(map[string]*ChainStats),
+	}
+}
+
+// healthStatus is a catch-all that grabs the latest update for a given chain.
+// The last segment of the URL is used to identify the chain (eg. "health/goerli" will return "goerli").
+func (s httpMetricServer) HealthStatus(w http.ResponseWriter, r *http.Request) {
+	// Get last segment of URL
+	chainName := path.Base(r.URL.String())
+	chain, ok := s.chains[chainName]
+	if !ok {
+		http.Error(w, "Invalid chain name", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	current := chain.LatestBlock()
+	prev := s.stats[chainName]
+	if s.stats[chainName] == nil {
+		// First time we've received a block for this chain
+		s.stats[chainName] = &ChainStats{
+			ChainId:     chain.Id(),
+			Height:      current.Height,
+			LastUpdated: current.LastUpdated,
+		}
+	} else {
+		now := time.Now()
+		timeDiff := now.Sub(prev.LastUpdated)
+		// If block has changed, update it
+		if current.Height.Cmp(prev.Height) == 1 {
+			s.stats[chainName].LastUpdated = current.LastUpdated
+			s.stats[chainName].Height = current.Height
+		} else if int(timeDiff.Seconds()) >= s.blockTimeout { // Error if we exceeded the time limit
+			http.Error(w, fmt.Sprintf("chain %d height hasn't changed for %f seconds. Current Height: %s", prev.ChainId, timeDiff.Seconds(), current.Height), http.StatusInternalServerError)
+			return
+		} else if current.Height != nil && prev.Height != nil && current.Height.Cmp(prev.Height) == -1 { // Error for having a smaller blockheight than previous
+			http.Error(w, fmt.Sprintf("unexpected block height. previous = %s current = %s", prev.Height, current.Height), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	err := json.NewEncoder(w).Encode(s.stats[chainName])
+	if err != nil {
+		log.Error("Failed to serve metrics")
+	}
+}
